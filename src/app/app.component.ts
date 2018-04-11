6e08cdeaaff1e06a90c08762fed80080d4362051
sahilgulatilanet <sahilgulati.lanetteam@gmail.com>
2018-04-11 16:38:27 +0530

    insert view update and delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,70 @@ import {MyserviceService} from "./myservice.service";
 export class AppComponent {
   title = 'CRud App';
   d;
+  fnm;
+  fad;
+  fid;
+  updBtn=false;
   constructor(private myservice:MyserviceService){
-    this.myservice.viewdata().subscribe((res)=>{
-      console.log(res);
-      this.d=res;
-    });
+    this.myservice.viewdata();
     //console.log(this.d);
   }
   onClickReg(data){
-    let user={
-      nm:data.nm,
-      ad:data.ad
+
+    if(this.updBtn){
+      let user={
+        nm:data.nm,
+        ad:data.ad,
+        id:data.fid
+      };
+      this.myservice.updateData(user).subscribe((res)=>{
+        alert(res['result']);
+        console.log(res);
+        this.updBtn=false;
+        this.myservice.viewdata();
+      });
+    }else {
+      let user={
+        nm:data.nm,
+        ad:data.ad
+      };
+      this.fnm="";
+      this.fad="";
+      this.myservice.adddata(user);
     }
-    this.myservice.adddata(user);
+    this.fnm="";
+    this.fad="";
+    this.fid="";
   }
+  select(){
+    this.myservice.viewdata();
+  }
+  onDeleteclk(val){
+    console.log(val);
+    let user={
+      id:val
+    };
+    this.myservice.deleteData(user);
+  }
+  onUpdateClk(val){
+    console.log(val);
+    let user={
+      id:val
+    };
+    this.myservice.updateDataclk(user).subscribe((res)=>{
+      console.log(res);
+      this.fnm=res['nm'];
+      this.fad=res['ad'];
+      this.fid=res['id'];
+      this.updBtn=true;
+
+    });
+
+  }
+  cancleUpdate(){
+    this.updBtn=false;
+    this.fnm="";
+    this.fad="";
+  }
+
 }
--- src/app/myservice.service.ts
+++ src/app/myservice.service.ts
@@ -4,7 +4,7 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable()
 export class MyserviceService {
-
+  data;
   constructor(private http:HttpClient) {
     /*let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json; charset=utf-8');
@@ -18,9 +18,26 @@ export class MyserviceService {
     headers = headers.set('Content-Type', 'application/json; charset=utf-8');*/
     return this.http.post("http://localhost/angularapi/insert_stud.php",data).subscribe((res)=>{
       alert(res['result']);
+      this.viewdata();
     });
   }
   viewdata(){
-    return this.http.get("http://localhost/angularapi/view_stud.php")
+    return this.http.get("http://localhost/angularapi/view_stud.php").subscribe((res)=>{
+      console.log(res);
+      this.data=res;
+    });
+  }
+  deleteData(data){
+    return this.http.post("http://localhost/angularapi/delete_stud.php",data).subscribe((res)=>{
+      console.log(res);
+      alert(res['result']);
+      this.viewdata();
+    });
+  }
+  updateDataclk(data){
+    return this.http.post("http://localhost/angularapi/update_data_stud.php",data);
+  }
+  updateData(data){
+    return this.http.post("http://localhost/angularapi/update_stud.php",data);
   }
 }
